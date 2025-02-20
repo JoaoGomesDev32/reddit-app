8f4a7c6eac684a70550477f3927c3ba5da54dad2
João Felipe <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-02-20 21:13:41 +0000

    ✨
    create initial page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts } from "../features/posts/postsSlice";
+import { RootState, AppDispatch } from "../store";
+import Post from "../components/Post";
+
+const Home: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { posts, status } = useSelector((state: RootState) => state.posts);
+
+  useEffect(() => {
+    dispatch(fetchPosts("popular"));
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Reddit App</h1>
+      {status === "loading" && <p>Carregando...</p>}
+      {status === "failed" && <p>Erro ao carregar posts.</p>}
+      {posts.map((post) => (
+        <Post key={post.id} {...post} />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
